ea0a04610234839abf8ded025165a871ba87202c
Evan Suma Rosenberg <suma@umn.edu>
2026-06-11 12:33:13 +0000

    updated vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   server: {
     host: '0.0.0.0',
+    allowedHosts: ['ray-tracer.exe.xyz'],
     port: 5173,
     strictPort: true,
   },
